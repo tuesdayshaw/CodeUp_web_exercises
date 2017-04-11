cceb022ddc3a91500b8e9c221f855fa453f87731
Tuesday Shaw <tuesday.shaw@gmail.com>
2017-04-11 10:53:12 -0500

    Completed Exercise 2 & 3

--- js/for_loop.js
+++ js/for_loop.js
@@ -13,3 +13,25 @@ for (var i = 0; i < 11; i++) {
 
 // Exercise 2:
 
+
+
+for (var i = 0; i < 11; i++) {
+    var randomNumber = Math.floor(Math.random() * 200)
+    if(randomNumber % 2 == 0)
+    {
+        console.log(randomNumber + ' is even');
+    }
+    else
+    {
+        console.log(randomNumber + ' is odd');
+    }
+
+}
+
+
+// Exercise 3:
+
+
+for (var i = 100; i > 0; i-=5) {
+    console.log(i);
+}
